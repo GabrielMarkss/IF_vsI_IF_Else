c37a46ac20849bafb535e9d81207cc3897de6543
Gabriel Marques <gabrielmqs1415@gmail.com>
2023-06-01 02:16:36 -0300

    01 de julho de 2023

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function competicao() {
+
+    var idade = parseInt(document.getElementById("idade").value);
+
+    if (idade >= 6 && idade <= 10) {
+        console.log("Infantil: " + idade + " anos")
+    } else if (idade >= 11 && idade <= 15) {
+        console.log("Infanto-Juvenil: " + idade + " anos")
+    } else if (idade >= 16 && idade <= 21) {
+        console.log("Juvenil:" + idade + " anos")
+    } else if (idade >= 22 && idade <= 35) {
+        console.log("Adulto:" + idade + " anos")
+    } else {
+        console.log("Atleta não apto a competir")
+    }
+
+}
